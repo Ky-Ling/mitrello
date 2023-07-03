696ff2ae17e6f6aad2e6edc71e0f34e89886320b
Ky-Ling <2845740149@qq.com>
2023-07-03 20:20:03 +0800

    feat: implement the search functionality

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,8 +4,14 @@ import React from 'react';
 import Image from 'next/image';
 import { MagnifyingGlassIcon, UserCircleIcon } from '@heroicons/react/24/solid';
 import Avatar from 'react-avatar';
+import { useBoardStore } from '@/store/BoardStore';
 
 const Header: React.FC = () => {
+	const [searchString, setSearchString] = useBoardStore((state) => [
+		state.searchString,
+		state.setSearchString,
+	]);
+
 	return (
 		<header>
 			<div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
@@ -27,6 +33,10 @@ const Header: React.FC = () => {
 						<MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
 						<input
 							type="text"
+							value={searchString}
+							onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+								setSearchString(e.target.value)
+							}
 							placeholder="Search"
 							className="flex-1 outline-none p-2"
 						/>
