a9e418c8c2e3faf3dd2eee2cd9c8cfdfdaf975bc
Ky-Ling <2845740149@qq.com>
2023-07-11 19:22:00 +0800

    feat: add env.example file

--- appwrite.ts
+++ appwrite.ts
@@ -3,7 +3,7 @@ import { Client, ID, Databases, Storage, Account } from 'appwrite';
 const client = new Client();
 
 client
-	.setEndpoint('https://cloud.appwrite.io/v1')
+	.setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
 	.setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!);
 
 const account = new Account(client);
